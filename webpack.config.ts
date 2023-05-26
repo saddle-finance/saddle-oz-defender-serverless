1beb68ea1731d08c4311ba544e8ee2b7b8630be7
Jongseung Lim <penandlim@gmail.com>
2023-05-26 13:26:09 -0400

    chore: Update dependencies, remove contributors, and adjust Webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,10 @@
 import glob from "glob";
 import path from "path";
+import dotenv from "dotenv";
+import webpack from "webpack";
+import nodeExternals from 'webpack-node-externals';
+
+dotenv.config();
 
 interface EntryObject {
   [key: string]: string;
@@ -16,7 +21,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name]/index.js",
-    libraryTarget: "commonjs2"
+    libraryTarget: "commonjs2",
   },
   target: "node",
   module: {
@@ -30,14 +35,22 @@ module.exports = {
   },
   optimization: {
     usedExports: true,
+    minimize: true, // Disable this to inspect the output better
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
   mode: "production",
-  externals: {
-    ethers: "commonjs2 ethers",
-    "defender-relay-client": "commonjs2 defender-relay-client",
-    "@ethersproject/providers": "commonjs2 @ethersproject/providers",
-  },
+  externals: [
+    nodeExternals({
+      // List the modules you want to include in the Webpack bundled output
+      allowlist: ["@cowprotocol/ts-dune-client", "ethcall", "abi-coder"],
+    }),
+  ],
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.DUNE_API_KEY": JSON.stringify(process.env.DUNE_API_KEY),
+      // Add more environment variables here, if needed
+    }),
+  ],
 };
